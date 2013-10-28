9d154157d70048e19d3fce4f834626288cdd03a3
xiong.shaole <xiong.shaole@dena.jp>
2013-10-28 18:00:20 +0800

    modify

--- tools/scpfile.sh
+++ tools/scpfile.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 # scpfile get|push <hostname> <filename>
+arr=( $@ )
 
 if [ $2 == 'dev02' ]; then
 	hostip="54.249.47.219" #等号两边都不能有空格，有空格就出错
@@ -20,29 +21,18 @@ fi
 
 #get files
 if [ $1 == 'get' ]; then
-	if [ ! $4 ]; then
-		echo scp -i ~/kr_white.pem mgsys@"$hostip":~/sg-gcard-kr/$3 ~/sg-gcard-kr/$3
-		scp -i ~/kr_white.pem mgsys@"$hostip":~/sg-gcard-kr/$3 ~/sg-gcard-kr/$3
-	else
-		echo scp -i ~/kr_white.pem mgsys@"$hostip":~/sg-gcard-kr/$3 ~/sg-gcard-kr/$4
-		scp -i ~/kr_white.pem mgsys@"$hostip":~/sg-gcard-kr/$3 ~/sg-gcard-kr/$4
-	fi
+	for i in "${arr[@]:2}"
+	do
+		echo scp -i ~/kr_white.pem mgsys@"${hostip}":~/sg-gcard-kr/${i} ~/sg-gcard-kr/${i}
+		scp -i ~/kr_white.pem mgsys@"${hostip}":~/sg-gcard-kr/${i} ~/sg-gcard-kr/${i}
+	done
 fi
 
 #push files
 if [ $1 == 'push' ]; then
-	arr=( $@ )
 	for i in "${arr[@]:2}"
 	do
 		echo scp -i ~/kr_white.pem ~/sg-gcard-kr/${i} mgsys@"${hostip}":~/sg-gcard-kr/${i}
 		scp -i ~/kr_white.pem ~/sg-gcard-kr/${i} mgsys@"${hostip}":~/sg-gcard-kr/${i}
 	done
 fi
-
-#open files
-if [ $1 == 'open' ]; then
-	echo scp -i ~/kr_white.pem mgsys@"$hostip":~/sg-gcard-kr/$3 ~/temp/
-	echo open ~/temp/"$filename"
-fi
-
-# echo scp -i ~/kr_white.pem mgsys@54.249.47.219:~/sg-gcard-kr/$1 ~/sg-gcard-kr/$2
